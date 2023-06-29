1f04a71abc8713186a5f8e1f1d31d8e7bfb2fcc5
Mehmet BAKİ <43927054+mehmettbaki@users.noreply.github.com>
2023-06-30 01:21:39 +0300

    Update global-data.js
    ADDED

--- utils/global-data.js
+++ utils/global-data.js
@@ -4,7 +4,7 @@ export const getGlobalData = () => {
     : 'Mehmet BAKİ';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'mehmet BAKİ NESTJS NETLİFY APP';
+    : 'BACKEND DEV';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'All rights reserved.';
